aadb3812bcacb1cf48efbbca8c8ffadeb0936c55
Steve <sdweldon4@gmail.com>
2019-01-14 23:16:39 -0500

    Added push notifications for iphone

--- EventApp/EventApp/App.xaml.cs
+++ EventApp/EventApp/App.xaml.cs
@@ -57,7 +57,7 @@ namespace EventApp
                 };
             }
 
-            AppCenter.Start("android=bcc3eb00-cbdb-4d66-82f7-860eb3b56e56", typeof(Push), typeof(Analytics));
+            AppCenter.Start("android=bcc3eb00-cbdb-4d66-82f7-860eb3b56e56;ios=296e7478-5c95-4aa1-b904-b43c78377d1c;", typeof(Push), typeof(Analytics));
 
         }
 
