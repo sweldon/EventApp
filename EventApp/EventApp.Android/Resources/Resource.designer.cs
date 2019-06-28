4e7b4d8ed00bd4883285af5d723ed552d5d22de0
sweldon <stmydchannel@gmail.com>
2019-06-27 20:40:43 -0400

    idk

--- EventApp/EventApp.Android/Resources/Resource.designer.cs
+++ EventApp/EventApp.Android/Resources/Resource.designer.cs
@@ -3051,14 +3051,11 @@ namespace EventApp.Droid
 			// aapt resource value: 0x7f020135
 			public const int notification_bg_normal_pressed = 2130837813;
 			
+			// aapt resource value: 0x7f020149
+			public const int notification_template_icon_low_bg = 2130837833;
+			
 			// aapt resource value: 0x7f020136
-			public const int notification_icon_background = 2130837814;
-			
-			// aapt resource value: 0x7f02014d
-			public const int notification_template_icon_bg = 2130837837;
-			
-			// aapt resource value: 0x7f02014e
-			public const int notification_template_icon_low_bg = 2130837838;
+			public const int notification_tile_bg = 2130837814;
 			
 			// aapt resource value: 0x7f020137
 			public const int notification_tile_bg = 2130837815;
@@ -3079,34 +3076,34 @@ namespace EventApp.Droid
 			public const int rewards = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int search = 2130837821;
+			public const int star = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int share = 2130837822;
+			public const int today_icon = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int splash = 2130837823;
+			public const int tomorrow_icon = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int splashscreen = 2130837824;
+			public const int tooltip_frame_dark = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int star = 2130837825;
+			public const int tooltip_frame_light = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int today_icon = 2130837826;
+			public const int trending = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int tomorrow_icon = 2130837827;
+			public const int up = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int tooltip_frame_dark = 2130837828;
+			public const int up_16 = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int tooltip_frame_light = 2130837829;
+			public const int up_active = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int trending = 2130837830;
+			public const int up_active_16 = 2130837830;
 			
 			// aapt resource value: 0x7f020147
 			public const int trophy = 2130837831;
